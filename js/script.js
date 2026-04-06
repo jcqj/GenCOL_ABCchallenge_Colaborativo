566f59976a5480c70ab83196ad891eac9ae51843
Juan Camilo Quevedo Jaimes <juanqueja64@gmail.com>
2026-04-06 15:09:17 -0500

    Actualizando contador JS

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const contenedorGaleria = document.getElementById("cuadricula-letras");
 const botonTodas = document.getElementById("boton-todas");
 const botonVocales = document.getElementById("botón-vocales");
 
+const contador = document.getElementById("contador");
+
 // selectCard.addEventListener('click', function () {
 //     console.log('Card clicked!');
 // });
@@ -18,5 +20,7 @@ function voltear(card) {
     if (card.classList.contains("volteada") && !letrasDescubiertas.has(letra)) {
         // Sumar al registro de descubiertas
         letrasDescubiertas.add(letra);
+        contador.textContent = contador.textContent;
+        contador.textContent++;
     }
 }
