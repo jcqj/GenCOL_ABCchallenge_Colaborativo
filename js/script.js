87d2f9c49c604cf524c884f00b85c53e3bf6e710
fabian1031 <lugia759@gmail.com>
2026-04-13 17:49:55 -0500

    update target

--- js/script.js
+++ js/script.js
@@ -7,20 +7,29 @@ const marcadorContador = document.getElementById("contador-numero");
 const contenedorGaleria = document.getElementById("cuadricula-letras");
 const botonTodas = document.getElementById("boton-todas");
 const botonVocales = document.getElementById("botón-vocales");
-
 const contador = document.getElementById("contador");
 
-// selectCard.addEventListener('click', function () {
-//     console.log('Card clicked!');
-// });
+
 
 function voltear(card) {
     card.classList.toggle('volteada');
     const letra = card.querySelector('.card-frente').textContent;
     if (card.classList.contains("volteada") && !letrasDescubiertas.has(letra)) {
-        // Sumar al registro de descubiertas
         letrasDescubiertas.add(letra);
         contador.textContent = contador.textContent;
         contador.textContent++;
     }
+}
+function filtrar(tipo) {
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    if (tipo === 'todas') {
+      card.style.display = 'block';
+    } else if (card.dataset.tipo === tipo) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
 }
